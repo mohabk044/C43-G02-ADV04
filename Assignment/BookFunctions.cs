f64c6bb8c14ffcb44cd728287e4e2d8cb2bdedcb
Mohab Khaled <mohabk044@gmail.com>
2025-01-23 17:18:18 +0200

    Part 2 - Q 01

---
+++ Assignment/BookFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class BookFunctions
+    {
+        public static string? GetTitle(Book book)
+        {
+            return book.Title;
+        }
+
+        public static string? GetAuthors(Book book)
+        {
+            return book.Authors;
+        }
+
+        public static decimal GetPrice(Book book)
+        {
+            return book.Price;
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part2 - Q 01
+            Book myBook = new Book("Q123kndskj2", "To Kill a Mockingbird", "Harper Lee", new DateTime(2023, 5, 1), 350);
+            Console.WriteLine(myBook.ToString());
+            #endregion
+        }
+    }
+}

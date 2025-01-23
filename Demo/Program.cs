31c12ceacff5552ac644d0b23d246f44320322d8
Mohab Khaled <mohabk044@gmail.com>
2025-01-23 16:51:27 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        public delegate int StringFuncDelegate(string str);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            #region Delegate-Example1
+            StringFuncDelegate reference;
+            reference = new StringFuncDelegate(String_Functions.GetCountOfUpperCaseChars);
+            reference -= new StringFuncDelegate(String_Functions.GetCountOfLowerCaseChars);
+            //reference = String_Functions.GetCountOfUpperCaseChars; //Syntsx Sugar
+            int count = reference.Invoke("Mostafa MOHamed");
+            // int count = reference("Hello World"); //Syntsx Sugar
+            Console.WriteLine(count);
+            #endregion
+        }
+    }
+}
---
+++ Demo/String_Functions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class String_Functions
+    {
+        public static int GetCountOfUpperCaseChars(string Name)
+        {
+            int count = 0;
+            if (Name is not null)
+            {
+                for (int i = 0; i < Name.Length; i++)
+                {
+                    if (char.IsUpper(Name[i]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        public static int GetCountOfLowerCaseChars(string Name)
+        {
+            int count = 0;
+            if (Name is not null)
+            {
+                for (int i = 0; i < Name.Length; i++)
+                {
+                    if (char.IsLower(Name[i]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
